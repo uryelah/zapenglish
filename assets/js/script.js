671ae402482c166fa7a7a92677a0a697ef1b4207
sarah <sarahchamorro@gmail.com>
2019-07-31 20:14:40 -0300

    http to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -370,7 +370,7 @@ if($('#map').length >0 ) {
       var map = new google.maps.Map(mapElement, mapOptions);
       var addresses = ['Brooklyn'];
       for (var x = 0; x < addresses.length; x++) {
-          $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+          $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
               var p = data.results[0].geometry.location
               var latlng = new google.maps.LatLng(p.lat, p.lng);
               new google.maps.Marker({
